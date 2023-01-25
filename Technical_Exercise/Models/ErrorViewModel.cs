d18693a551d9bb5a07e14cd58b0f1def918c119b
ludingcheng <ludingcheng970804@163.com>
2023-01-25 12:14:00 +1100

    first commit

---
+++ Technical_Exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Technical_Exercise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
