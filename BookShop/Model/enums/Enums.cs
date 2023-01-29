3ac6436202708aba70dc1792b90f9a575e8402de
Tommygiant <78850099+Tommygiant@users.noreply.github.com>
2023-01-29 13:41:42 +1100

    v0.3

---
+++ BookShop/Model/enums/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.enums
+{
+    public enum LoginResult
+    {
+        用户名不存在 = 0,
+        密码错误 = 1,
+        用户已被冻结 = 2,
+        登录成功 = 3
+    }
+
+    public enum RegisterResult
+    {
+        用户名已存在 = 0,
+        注册成功 = 1,
+        注册失败 = 2,
+    }
+
+    public enum SortType
+    {
+        
+        PriceAsc = 1,
+        PriceDesc = 2,
+       
+        OnsaleDesc = 4
+    }
+}
