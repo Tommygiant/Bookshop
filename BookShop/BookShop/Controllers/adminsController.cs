f3b32a2537000c3f227153192934d01e9d7965b5
ludingcheng <ludingcheng970804@163.com>
2023-01-29 12:10:35 +1100

    v0.2

---
+++ BookShop/BookShop/Controllers/adminsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+
+namespace BookShop.Controllers
+{
+    public class adminsController : Controller
+    {
+        private BOOKSHOPEntities db = new BOOKSHOPEntities();
+
+        // GET: admins
+        public ActionResult Index()
+        {
+            if (Session["adminusername"] == null)
+            {
+                return Content("<script>alert('Please Login');location.href='/Home/adminLogin';</script>");
+            }
+            return View(db.admin.ToList());
+        }
+
+        // GET: admins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "admin_id,admin_name,admin_pwd,admin_email")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.admin.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "admin_id,admin_name,admin_pwd,admin_email")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: admins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            admin admin = db.admin.Find(id);
+            db.admin.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookShop/BookShop/Controllers/ordersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+
+namespace BookShop.Controllers
+{
+    public class ordersController : Controller
+    {
+        private BOOKSHOPEntities db = new BOOKSHOPEntities();
+
+        // GET: orders
+        public ActionResult Index()
+        {
+            var orders = db.orders.Include(o => o.books);
+            return View(orders.ToList());
+        }
+
+        // GET: orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orders orders = db.orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orders);
+        }
+
+        // GET: orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.book_id = new SelectList(db.books, "book_id", "ISBN");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "order_id,username,order_date,book_id,book_title,quantity,adress,status,price")] orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.orders.Add(orders);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.book_id = new SelectList(db.books, "book_id", "ISBN", orders.book_id);
+            return View(orders);
+        }
+
+        // GET: orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orders orders = db.orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.book_id = new SelectList(db.books, "book_id", "ISBN", orders.book_id);
+            return View(orders);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "order_id,username,order_date,book_id,book_title,quantity,adress,status,price")] orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orders).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.book_id = new SelectList(db.books, "book_id", "ISBN", orders.book_id);
+            return View(orders);
+        }
+
+        // GET: orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orders orders = db.orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orders);
+        }
+
+        // POST: orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            orders orders = db.orders.Find(id);
+            db.orders.Remove(orders);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
